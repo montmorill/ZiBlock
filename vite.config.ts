aedba96e7c898064bd6fdbe5865b744ea9d18823
montmorill <montmorill@outlook.com>
2026-07-16 00:25:03 +0800

    feat: add GITHUB_REPOSITORY environment variable for build and update base path in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import yaml from '@modyfi/vite-plugin-yaml'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte(), yaml()],
+  base: process.env.GITHUB_REPOSITORY 
+    ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/` 
+    : '/ZiBlock/',
 })
