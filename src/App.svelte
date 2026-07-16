8f540674403be5c37e9bfb98df96a2c970eae996
montmorill <montmorill@outlook.com>
2026-07-16 18:08:26 +0800

    fix: update Blockly locale settings to include default variable name

--- src/App.svelte
+++ src/App.svelte
@@ -6,7 +6,9 @@
 	import { Generator } from "./lib/generator";
 	import { registerFieldMultilineInput } from "@blockly/field-multilineinput";
 
-	Blockly.setLocale(zhHans as any);
+	Blockly.setLocale(Object.assign({}, zhHans, {
+		VARIABLES_DEFAULT_NAME: "变量",
+	}) as any);
 
 	registerFieldMultilineInput();
 
