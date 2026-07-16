361653715242bf3bf272458ca39cdc2ea7eaf174
montmorill <montmorill@outlook.com>
2026-07-16 09:38:59 +0800

    feat: add evaluation result display and error handling in Blockly integration

--- src/App.svelte
+++ src/App.svelte
@@ -24,12 +24,33 @@
 			}
 		});
 	});
+
+	let evaluateResult = $state("");
+	let evaluateError = $state("");
+	$effect(() => {
+		fetch(`https://tianzi.pbhh.net/translate`, {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({ text: outputCode }),
+		})
+			.then((res) => res.json())
+			.then(async (res) => {
+				evaluateResult = res.translated;
+				evaluateError = "";
+			})
+			.catch((error) => {
+				evaluateError = "Error: " + (error.message || error);
+			});
+	});
 </script>
 
 <section>
 	<div id="blocklyDiv" bind:this={blocklyDiv}></div>
 	<pre id="outputDiv">
 		<code class={errorMsg && "error"}>{errorMsg || outputCode}</code>
+	</pre>
+	<pre id="resultDiv">
+		<code class={evaluateError && "error"}>{evaluateError || evaluateResult}</code>
 	</pre>
 </section>
 
@@ -42,6 +63,14 @@
 	#outputDiv {
 		position: fixed;
 		top: 0;
+		right: 0;
+		margin: 0.5em;
+		user-select: none;
+	}
+
+	#resultDiv {
+		position: fixed;
+		bottom: 0;
 		right: 0;
 		margin: 0.5em;
 		user-select: none;
